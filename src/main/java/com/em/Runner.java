29b78f65d39e25f488d304feefe418798c2b6c6f
Ayse <140872689+ayseteas@users.noreply.github.com>
2023-09-19 13:49:16 -0600

    We completed the Runner Class.

--- src/main/java/com/em/Runner.java
+++ src/main/java/com/em/Runner.java
@@ -1,11 +1,56 @@
 package com.em;
 
+import java.util.Scanner;
+
 public class Runner {
 
     public static void main(String[] args) {
 
         EmployeeRepository employeeRepository = new EmployeeRepository();
         employeeRepository.createTable();
+
+        Scanner scan = new Scanner(System.in);
+        int select;
+
+
+        do{
+            System.out.println("-----------------------------------");
+            System.out.println(" Employee Management System ");
+            System.out.println("1. Register an Employee");
+            System.out.println("2. List All Employee");
+            System.out.println("3. Update an Employee");
+            System.out.println("4. Delete an Employee By ID");
+            System.out.println("0. Exit");
+            System.out.println("Select An Activity");
+            System.out.println("-----------------------------------");
+
+             select = scan.nextInt();
+             scan.nextLine();
+
+             switch (select)
+             {
+                 case 1:
+                     //Register an Employee
+                     break;
+                 case 2:
+                     //List All Employee
+                     break;
+                 case 3:
+                     //Update an Employee
+                     break;
+                 case 4:
+                     //Delete an Employee by ID
+                     break;
+                 case 0:
+                     //Exit
+                     System.out.println("Thank you for using the App. Have a good day!");
+                     break;
+                 default:
+                     System.out.println("Invalid Input. Try again");
+                     break;
+             }
+
+        }while(select != 0);
 
     }
 
